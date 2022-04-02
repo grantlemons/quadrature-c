75a32a20048d67962607ceafb094fe5052b620cc
Grant Lemons <grant_lemons@yahoo.com>
2022-04-01 21:36:11 -0500

    move function declarations to header file

---
+++ quadrature.h
@@ -0,0 +1,9 @@
+double f(float x);
+double left_sum(float, float, int);
+double right_sum(float, float, int);
+double midpoint_sum(float, float, int);
+double trapezoidal_rule(float, float, int);
+double simpsons_rule(float, float, int);
+double simpsons_38rule(float, float, int);
+double booles_rule(float, float, int);
+double gaussian_quad(float, float, int);
